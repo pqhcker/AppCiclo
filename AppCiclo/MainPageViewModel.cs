6fc30e4dc2920f5b397526e440f4279e3d2ed20e
Alejandro Solano <alejandro.solano@freshconsulting.com>
2022-01-27 00:25:30 -0600

    Initial release
    Initial Release

---
+++ AppCiclo/MainPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppCiclo
+{
+    public class MainPageViewModel : ObservableObject
+    {
+        private int contador;
+
+        public MainPageViewModel()
+        {
+            EjecutarCommand = new(async () => await EjecutarContador());
+        }
+
+        private async Task EjecutarContador()
+        {
+            for(int i =1; i <= 10000; i++)
+            {
+                Contador = i;
+                await Task.Delay(100);
+            }
+        }
+
+        public int Contador
+        {
+            get => contador;
+            set => SetProperty(ref contador, value);
+        }
+
+        public Command EjecutarCommand { get; set; }
+
+    }
+}
